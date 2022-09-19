ff763df20464083679b6f42103ded32e377ea149
PreciousDipe <preciousdipe@gmail.com>
2022-09-19 22:45:24 +0000

    array

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,13 @@
  * @str: string
  * Return: Nothing
  */
-void print_rev(char *s)
+void print_rev(char *str)
 {
 	int len;
 
-	for (len = 0; s[len] != '\0'; len++)
+	for (len = 0; str[len] != '\0'; len++)
 		;
 	for (len--; len >= 0; len--)
-		putchar(s[len]);
+		putchar(str[len]);
 	putchar(10);
 }
