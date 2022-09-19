e603d85ff6ec75c1953d8539d7ece79c17a3686d
PreciousDipe <preciousdipe@gmail.com>
2022-09-19 23:41:43 +0000

    array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,18 @@
  */
 void puts_half(char *str)
 {
-	int index = 0, len = 0;
+	int index = 0, len = 0, n;
 
 	while (str[index++])
 		len++;
 
-	for (index = 5; index < len; index++)
+	if ((len % 2) == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
 		putchar(str[index]);
 	putchar(10);
 }
