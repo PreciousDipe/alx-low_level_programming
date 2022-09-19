bb56680e106c32092648e5a4dcb1c8b7fdce1998
PreciousDipe <preciousdipe@gmail.com>
2022-09-19 19:25:13 +0000

    array

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar(10);
+}
