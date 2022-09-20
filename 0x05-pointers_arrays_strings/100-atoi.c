0b76626f92908145c66999c621e2e14773152213
PreciousDipe <preciousdipe@gmail.com>
2022-09-20 00:23:06 +0000

    array

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - converts strings to integer
+ * @s: string
+ * Return: Always 0
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	}
+	while (*s++);
+
+	return (num * sign);
+}
+
