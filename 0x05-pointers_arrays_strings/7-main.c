6ab298acecc91ee0949df5fe198097b0b1c09b05
PreciousDipe <preciousdipe@gmail.com>
2022-09-22 20:22:15 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - chech the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-low_level_programming
+This is a read me file
