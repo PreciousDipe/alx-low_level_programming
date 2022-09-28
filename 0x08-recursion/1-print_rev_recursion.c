82cf1bfd5f50545c61474e30d2a19d5311f9133b
PreciousDipe <preciousdipe@gmail.com>
2022-09-28 20:12:58 +0000

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: a string
+ * Return: a string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion((s + 1));
+		putchar(*(s + 0));
+	}
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of a 
+ * string
+ * @s: a string
+ * Return: a string
+ */
+int _strlen_recursion(char *n)
+{
+	if (*n == '\0')
+		return (0);
+	return (1 + _strlen_recursion((n + 1)));
+}
