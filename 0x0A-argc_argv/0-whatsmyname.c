e692dbc90ae715196e9a7dd10081608e01b3e16f
PreciousDipe <preciousdipe@gmail.com>
2022-09-30 16:17:53 +0000

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints a program name
+ * @argc: counts arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
